f39242f42e74909d887fc765b9f0ad07bf17a3b5
Silas Jimmy <jimmysilas17@gmail.com>
2022-01-11 14:42:44 +0300

    refactor: add title mixin

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import titleMixin from './mixins/titleMixin'
+
+// Install mixins
+Vue.mixin(titleMixin)
 
 Vue.config.productionTip = false
 
